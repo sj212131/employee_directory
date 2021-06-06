a786395ce601d8d9d9f065af835accd44e2bf1a4
shangxian Piao <sj212131@hotmail.com>
2021-06-06 12:45:22 -0400

    upload api.js table.filter updated app.js and index.js with bootrap

--- employeedir/src/App.js
+++ employeedir/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import FilterInput from "./FilterInput";
+import Table from "./Table";
+import { getUsers } from "./API";
+import "./App.css";
 
 function App() {
+  const [initialUsers, updateAvailableUsers] = useState([]);
+  const [usersToRender, updateUsersToRender] = useState([]);
+
+  useEffect(() => {
+    getUsers().then(({ data: { results } }) => updateAvailableUsers(results));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee Directory</h1>
+      <p>
+        To filter by first name, please begin your search in the user input
+        below
+      </p>
+      <FilterInput users={initialUsers} updateUsers={updateUsersToRender} />
+      <Table users={usersToRender} />
     </div>
   );
 }
